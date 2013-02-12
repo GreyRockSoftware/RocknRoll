cf49e1d46152ebd8c00e620ac6a0ff4aad43a32d
Ryan Moore <ryan.moore@greyrocksoft.com>
2013-02-12 11:08:40 -0700

    Adding default rails framework

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RocknRoll::Application.config.secret_token = 'bfec3580de9341a74795eaa744d811f785d86ce03a8b5dec1793594c2bef975caf09625d69d47ae9a719a3933dfdc1bf052642002abe8f1d632152c94925efc6'
